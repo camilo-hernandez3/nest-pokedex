ab2c5455e410efb443bcd369f465bee551d3acb8
camilo hernandez <ch4602117@gmail.com>
2023-04-27 08:42:01 -0500

    Fin seccion 7

---
+++ src/main.ts
@@ -0,0 +1,21 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+
+  /* añade el segmento */
+  app.setGlobalPrefix('api/v2');
+
+  /* configurar validator para controller */
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+    }),
+  );
+
+  await app.listen(3000);
+}
+bootstrap();
